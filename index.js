63ef9c03371631e6c90c8a829b5c2bdde9f60e7b
Son <son@chillbits.com>
2022-04-17 13:15:21 +0000

    Show blockchain info only

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 const express = require('express')
 const app = express()
-const { blockchainInfo, progress } = require('./blockchain')
 
 app.set('view engine', 'pug')
 
 app.get('/', (req, res) => {
+  const { blockchainInfo, progress } = require('./blockchain')
 
   res.render('index', {
-    title: 'Marie',
-    message: 'Xin chào anh em TGISS, mình là Marie, mình được 2 tuần tuổi, có nhiệm vụ canh giữ node Bitcoin cho đại ca Sơn.',
-    message2: 'Anh em nhìn thấy page này có nghĩa là Bitcoin node đang chạy bình thường. Mình sẽ update thông tin của node lên đây sớm.',
-    blockcount: 'Số lượng block đã load trên Marie hiện tại: ' + blockchainInfo.blocks + '/' + blockchainInfo.headers + ' (' + progress + '%)'
+    title: 'Marie Bitcoin Full Node',
+    loaded_progress: 'Số lượng blocks đã load: ' + blockchainInfo.blocks + '/' + blockchainInfo.headers + ' (' + progress + '%)',
+    size_on_disk: 'Dung lượng của node: ' + Number(blockchainInfo.size_on_disk / 1024 / 1024 / 1024).toPrecision(4) + ' GB'
   })
 })
 
